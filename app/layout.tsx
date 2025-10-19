4cd6c37272a2a83e94c3633038844b551c50d062
NerdyCo <kautsarteguh2002@gmail.com>
2025-10-19 07:20:43 +0700

    add icon to head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en" className={robotoMono.variable}>
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://img.icons8.com/material-outlined/24/pixel-cat.png"
+        ></link>
         <title>Portfolio</title>
       </head>
       <body>{children}</body>
